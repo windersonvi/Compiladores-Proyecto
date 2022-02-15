0131350bd1f391c8862c3024a1f3ee050b5744f1
windersonvi <windersonvi@gmail.com>
2022-02-14 22:43:16 -0400

    Actulizacion

--- src/proyecto/compiladores/ProyectoCompiladores.java
+++ src/proyecto/compiladores/ProyectoCompiladores.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyecto.compiladores;
-
-/**
- *
- * @author ganda
- */
 public class ProyectoCompiladores {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Hola"); 
     }
     
 }
